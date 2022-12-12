5a1c4c9b889125132583e1ef1cec9c4e8c1fdd45
MIN <65863834+keimindev@users.noreply.github.com>
2022-12-12 22:05:48 +0900

    Time: 369 ms (18.99%), Space: 50.9 MB (45.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+    //배열을 정리해서 
     nums = nums.sort();
     for (let i=0; i<nums.length; i++){
        if(nums[i] == nums[i+1]){
