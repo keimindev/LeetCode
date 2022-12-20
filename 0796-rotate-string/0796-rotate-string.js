035e84a0c365ea96ca78bd50e5884f2dd43cb244
MIN <65863834+keimindev@users.noreply.github.com>
2022-12-20 23:43:28 +0900

    Time: 101 ms (27.77%), Space: 42.2 MB (43.66%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+      for(let i = 0; i < s.length; i++) {
+        if (s.slice(i) + s.slice(0,i) === goal) return true;
+    }
+    return false;
+};
