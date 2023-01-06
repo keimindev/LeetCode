2aceb3b1364295f02fb984cbfd0ac8472a5237d1
MIN <65863834+keimindev@users.noreply.github.com>
2023-01-06 23:09:34 +0900

    Time: 125 ms (33.59%), Space: 46.2 MB (49.38%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  if(s == ' ') return true
+  let temp = s.replace(/[^a-zA-Z0-9ㄱ-힣]/g, '').toLowerCase()
+ //  let lowerS = temp.split(' ').join(',')
+  let len = Math.floor([...temp].length / 2)
+  let first = [...temp].splice(0,len)
+  let second = [...temp].reverse().splice(0,len)
+    
+  let result = 0
+  for(let i=0; i<first.length; i++){
+      first[i] == second[i] ? 0 : result++
+  }
+  
+    return result > 0 ? false : true
+};
