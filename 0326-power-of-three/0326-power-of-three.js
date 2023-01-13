bf2fb4cd92e669f9f7bb5da4691678abb3752f93
MIN <65863834+keimindev@users.noreply.github.com>
2023-01-13 22:45:54 +0900

    Time: 187 ms (97.25%), Space: 50.6 MB (93.07%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n <= 0) {
+    return false;
+  }
+  if (n == 1) {
+    return true;
+  }
+
+  let result = n;
+
+  while (result > 3) {
+    result /= 3;
+  }
+
+  if (result % 3 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
