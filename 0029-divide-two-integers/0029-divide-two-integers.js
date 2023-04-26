68124803deda8a9c8d842b2f62ed5766a3228b8e
MIN <65863834+keimindev@users.noreply.github.com>
2023-04-26 23:57:40 +0900

    Time: 83 ms (36.24%), Space: 43.8 MB (76.22%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.js
+++ 0029-divide-two-integers/0029-divide-two-integers.js
@@ -5,9 +5,10 @@
  */
 var divide = function(dividend, divisor) {
  let result = parseInt(dividend / divisor);
-    
-    if (result > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
-    if (result < Math.pow(2, 31) * -1) return Math.pow(2, 31) * -1;
+
+    //나눈 값이 Integer 범위를 벗어나면 그 마지막 범위를 찾아서 리턴
+ if (result > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+ if (result < Math.pow(2, 31) * -1) return Math.pow(2, 31) * -1;
 
     return result;
 };
