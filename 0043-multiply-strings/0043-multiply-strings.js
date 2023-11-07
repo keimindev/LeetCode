31253630ac3966af247ca41e64459d5af9c55a70
MIN <65863834+keimindev@users.noreply.github.com>
2023-11-07 14:49:33 +0900

    Time: 48 ms (93.02%), Space: 41.9 MB (91.54%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,6 +4,8 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
+    //BigInt ? 
+    //원시 값이 안정적으로 나타낼 수 있는 최대치인 2^53 - 1보다 큰 정수를 표현할 수 있는 내장 객체
     const multiplyNum = BigInt(num1) * BigInt(num2)
     return String(multiplyNum)
 };
