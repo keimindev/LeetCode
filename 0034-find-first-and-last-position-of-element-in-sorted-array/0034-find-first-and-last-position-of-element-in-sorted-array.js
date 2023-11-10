af70a2128c797dfb2188e72fabdd1012fba0f06f
MIN <65863834+keimindev@users.noreply.github.com>
2023-11-10 10:10:20 +0900

    Time: 61 ms (12.02%), Space: 42.8 MB (18.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,18 +6,25 @@
 var searchRange = function(nums, target) {
     let temp =[]
    
+    // 주어진 인지의 길이가 0일 때 
     if(nums.length == 0) {
         return [-1, -1]
+        
+    // 인자의 길이가 0이 아닐 때
     }else{
-    
+        
+        //선형검색으로 돌면서 해당 target 인덱스 찾아서 array에 넣어주기
     for(let i=0; i<nums.length; i++ ){
         if(nums[i] === target){
             temp.push(i)
         }
     }
     
+    // 만약에 맞는게 없어서 인덱스가 안 들어갔다면 
     if(temp.length == 0){
         temp= [-1, -1]
+        
+    // 들어간 인덱스가 있다면 제일 처음꺼랑 제일 끝부분만 찾아서 넣어주기 
     }else{
         temp = [temp[0], temp[temp.length-1]]
     }
