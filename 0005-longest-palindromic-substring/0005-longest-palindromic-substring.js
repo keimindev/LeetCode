0897a67e9abd054dc5430e0e6f1864154e193aa7
MIN <65863834+keimindev@users.noreply.github.com>
2023-11-24 12:31:47 +0900

    Time: 65 ms (90.70%), Space: 43.2 MB (87.87%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+ let maxStart = 0,
+    maxEnd = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let start = i,
+      end = i;
+    while (0 <= start && end < s.length && s[start] === s[end]) {
+      if (end - start > maxEnd - maxStart) {
+        maxStart = start;
+        maxEnd = end;
+      }
+      start--;
+      end++;
+    }
+
+    (start = i), (end = i + 1);
+    while (0 <= start && end < s.length && s[start] === s[end]) {
+      if (end - start > maxEnd - maxStart) {
+        maxStart = start;
+        maxEnd = end;
+      }
+      start--;
+      end++;
+    }
+  }
+
+  return s.slice(maxStart, maxEnd + 1);
+}
