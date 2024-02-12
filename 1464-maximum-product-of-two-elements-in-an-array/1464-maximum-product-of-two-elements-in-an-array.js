8afd5f588ff8335442a51bd513f8b607f0a45db8
MIN <65863834+keimindev@users.noreply.github.com>
2024-02-12 22:16:24 +0900

    Time: 55 ms (62.39%), Space: 50.6 MB (5.26%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -5,6 +5,7 @@
 var maxProduct = function(nums) {
     // max 뽑고 
     // max 인덱스빼고 다시 뽑고 ?
+    // 라고 생각했지만 내림차순으로 정렬하면 최고점 수 두개만 뽑아서 만들면 된다고 생각함. 
     
     const arr = nums.sort((a,b) => b-a)
    
