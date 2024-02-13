56f86575b41da4690dfcd6268eae7c63303d0333
MIN <65863834+keimindev@users.noreply.github.com>
2024-02-13 23:51:45 +0900

    Time: 104 ms (5.07%), Space: 55.6 MB (5.23%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -3,14 +3,40 @@
  * @return {number}
  */
 var diagonalSum = function(mat) {
-    const len = mat.length;
-    let sum = 0;
+    const sum = [];
     
-    mat.forEach((row, i) => {
-        const l = i;
-        const r = len - i - 1;
-        sum += l === r ? row[l] : row[l] + row[r];
-    });
+    let startRow = 0;
+    let endRow = mat.length - 1;
+    let startCol = 0;
+    let endCol = mat.length -1;
     
-    return sum;
+    while(startRow <= endRow && startCol <= endCol){
+        for(let i = startRow; i<=endRow; i++){
+            if(startRow === startCol || endRow === endCol){
+                sum.push(mat[startRow][i])
+            }
+            
+            startRow++
+       }
+    
+         
+        // row를 한번 초기화시켜줘야함.. 
+        startRow = 0
+        for(let i = endCol; i>=startCol; i--){
+            // 중간에 겹쳐지는 row와 column값을 제거하기 위해서 
+            if(endCol != startRow && endRow != startCol){
+                console.log(endCol, endRow,startRow,)
+                sum.push(mat[i][startRow])
+                
+            }
+            endCol--
+            startRow++
+        }
+        
+        const add = sum.reduce((acc, cur) => acc += cur)
+        console.log(sum, add)
+        return add
+        
+        
+    }
 };
