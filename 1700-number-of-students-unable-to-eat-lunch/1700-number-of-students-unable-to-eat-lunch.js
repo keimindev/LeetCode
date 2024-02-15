3758e227166567713e2f9447bc791708d8f66342
MIN <65863834+keimindev@users.noreply.github.com>
2024-02-15 23:50:30 +0900

    Time: 58 ms (37.01%), Space: 48.8 MB (30.34%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    
+    while(sandwiches.length !== 0){
+           if(students[0] != sandwiches[0]){
+               students.push(students.shift())
+           }else{
+               students.shift()
+               sandwiches.shift()
+           }
+        
+        if(!students.includes(sandwiches[0])){
+            return students.length;
+        }
+       
+    }
+     return students.length;
+    
+    
+    
+};
