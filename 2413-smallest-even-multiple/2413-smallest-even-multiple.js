16ae7a5b6b41f52f65510e618298c3b8ac1f9407
MIN <65863834+keimindev@users.noreply.github.com>
2024-05-09 22:51:39 +0900

    Time: 56 ms (29.43%), Space: 48.5 MB (74.67%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    if(n == 1){
+        return 2;
+    }
+    
+    if(n % 2 == 0){
+         return n;
+     };
+    
+    while(n == true){
+        if(n%2 == 0){
+             return n;
+        }else {
+            n = n 
+        }
+         
+      
+   
+        
+    };
+     return n* 2;   
+};
