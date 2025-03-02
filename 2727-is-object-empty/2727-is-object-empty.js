431803f24598513bc7d207aa884e2f26428f8982
MIN <65863834+keimindev@users.noreply.github.com>
2025-03-01 20:18:34 -0500

    Time: 47 ms (73.11%), Space: 52 MB (18.46%) - LeetHub

--- 2727-is-object-empty/2727-is-object-empty.js
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -1,8 +1,7 @@
 /**
- * @param {Object | Array} obj
+ * @param {Object|Array} obj
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-   for (const _ in obj) return false;
-    return true;
+    return Object.keys(obj).length === 0
 };
