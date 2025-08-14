61fabb37a087a2e499b3f61561620e82433ea176
Keira Jeong <65863834+keimindev@users.noreply.github.com>
2025-08-13 21:01:30 -0400

    Time: 0 ms (100%), Space: 53.1 MB (85.02%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+     const dummy = new ListNode();
+    dummy.next = head;
+
+    let current = dummy;
+
+    while (current.next && current.next.next) {
+        let first = current.next;
+        let second = current.next.next;
+
+        first.next = second.next;
+        second.next = first;
+        current.next = second;
+        current = first;
+    }
+
+    return dummy.next;   
+};
