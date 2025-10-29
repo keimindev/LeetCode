877a8422db488ec2458077f8c38afaabaefb263f
Keira Jeong <65863834+keimindev@users.noreply.github.com>
2025-10-29 06:38:59 -0400

    Time: 2 ms (88.87%), Space: 58.5 MB (89.93%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let result = Number.MAX_SAFE_INTEGER
+    let sum = 0, left = 0
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i]
+        while(sum >= target){
+            result = Math.min(result, i - left + 1)
+            sum -= nums[left++]
+        }
+    }
+    return result === Number.MAX_SAFE_INTEGER ? 0 : result 
+};
+
